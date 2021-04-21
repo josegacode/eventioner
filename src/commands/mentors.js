4510a3e1b45f76d3524969576b4cfd99778e433f
Jose Garcia <josegar.dev@gmail.com>
2021-04-21 16:24:19 -0500

    Can grant Mentor permission giving the member email

--- src/commands/mentors.js
+++ src/commands/mentors.js
@@ -26,18 +26,42 @@ module.exports = client => {
   command(client, 'mentor', message => {
     // Type of action
     const request = message.content.replace('!mentor ', '');
+
+    // Type of action that the user wants to perform
     const action = request.split(' ')[0];
+
+    // Params that will be used with the command
     const params = request.split(' ').slice(1);
-    const username = message.author.username;
+
+    // User who executed the message
+    const user = message.author;
+
+    // User member 
+    const member = message.guild.members.cache.find((member) => member.id === user.id);
+
+    // DEBUG
+    //console.log(JSON.stringify(member.roles, null, 4));
 
     switch(action) {
+      /**
+        * Lets the member be a Mentor by giving 
+        * its e-mail
+        * */
       case 'enroll':
         // Await for spreadsheet api
         //spreadsheetHandler.getInfo(config.spreadsheets.mentors);
         spreadsheetHandler.saveMentorEmail(config.spreadsheets.mentors, params[0]);
 
+        console.log(`Bot manage roles permissions: ${message.guild.me.hasPermission('MANAGE_ROLES')}`);
+        console.log(`Member manage roles permissions: ${member.hasPermission('MANAGE_ROLES')}`);
+        
+        //console.log(`Roles of ${member}: ${member.roles}`);
+        member.roles.add('828742601137717290');
+
+
+        // Feedback to member in discord channel
         const userFeedback = new MessageEmbed()
-          .setTitle(`Hey @${username}, you are now a Mentor! 👩‍🏫👨‍🏫`)
+          .setTitle(`Hey @${user.username}, you are now a Mentor! 👩‍🏫👨‍🏫`)
           .setDescription(`Thanks, your email was registered successfully and Mentor
             role was given to you, enjoy it ⚡`
           )
