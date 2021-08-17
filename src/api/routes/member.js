abeaea91d5bcecf4753c0f044e0959f3f01324aa
José García <josegarcia.dev@gmail.com>
2021-08-17 12:19:51 -0500

    App was containerized successfully

--- src/api/routes/member.js
+++ src/api/routes/member.js
@@ -2,25 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { client } = require('../../index');
 
-// define the home page route
-router.get('/', (req, res) => {
+const getGuild = () => {
 	const guild = client.guilds.cache.find(guild => {
 		return guild.id === process.env.MAIN_SERVER_ID;
-	})
+	});
 
+	return guild;
+}
+
+// define the home page route
+router.get('/', (req, res) => {
 	res
 		.status(200)
-		.json(guild.members.cache);
+		.json(getGuild().members.cache);
 });
 
 // Returns roles of certain user
 // todo: check for non id provided
 router.get('/:id/roles', (req, res) => {
-	const guild = client.guilds.cache.find(guild => {
-		return guild.id === process.env.MAIN_SERVER_ID;
-	})
-
-	const member = guild.members.cache
+	const member = getGuild().members.cache
 		.find(member => member.id === req.params.id);
 
 	res.json(member.roles.cache);
